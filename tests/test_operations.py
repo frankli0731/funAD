dcb448f3c93bec6bbc00e8ba25d365b3912adf47
Xu Tang <xutang@g.harvard.edu>
2022-11-20 01:21:59 -0500

    Add more test cases for operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -35,7 +35,70 @@ class TestOperations():
         # test exp operator
         d2 = op.exp(d1)
         assert d2.real == np.exp(real1) and d2.dual == np.exp(real1)*dual1
-    
+        
+    def test_log(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test log operator
+        d2 = op.log(d1)
+        assert d2.real == np.log(real1) and d2.dual == 1/real1*dual1
+
+    def test_tan(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test tan operator
+        d2 = op.tan(d1)
+        assert d2.real == np.tan(real1) and d2.dual == 1/np.cos(real1)**2*dual1
+
+    def test_arcsin(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test arcsin operator
+        d2 = op.arcsin(d1)
+        assert d2.real == np.arcsin(real1) and d2.dual == 1/np.sqrt(1-real1**2)*dual1
+
+    def test_arccos(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test arccos operator
+        d2 = op.arccos(d1)
+        assert d2.real == np.arccos(real1) and d2.dual == -1/np.sqrt(1-real1**2)*dual1
+
+    def test_arctan(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test arctan operator
+        d2 = op.arctan(d1)
+        assert d2.real == np.arctan(real1) and d2.dual == 1/(1+real1**2)*dual1
+
+    def test_sinh(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test sinh operator
+        d2 = op.sinh(d1)
+        assert d2.real == np.sinh(real1) and d2.dual == np.cosh(real1)*dual1
+
+    def test_cosh(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test cosh operator
+        d2 = op.cosh(d1)
+        assert d2.real == np.cosh(real1) and d2.dual == np.sinh(real1)*dual1
+
+    def test_tanh(self):
+        real1=1
+        dual1=2
+        d1=DualNumber(real1,dual1)
+        # test tanh operator
+        d2 = op.tanh(d1)
+        assert d2.real == np.tanh(real1) and d2.dual == 1/np.cosh(real1)**2*dual1
 
 if __name__ == "__main__":
     pass
