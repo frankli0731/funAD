11c373b866cb342138380b5f82520aef1e60790c
Xu Tang <xutang@g.harvard.edu>
2022-12-07 02:05:49 -0500

    Add spqrt, sigmoid and fix log test cases for operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -118,17 +118,47 @@ class TestOperations():
         assert d3 == np.tanh(real1)
 
     def test_log(self):
-        real1=7
-        dual1=8
+        real1=2
+        dual1=1
         d1=DualNumber(real1,dual1)
-        
         # test log operator
         # natural log
         d2 = op.log(d1)
         assert d2.real == np.log(real1) and d2.dual == dual1/real1
         # base specified
-        d3 = op.log(d2,base=1.1)
-        assert d2.real == np.log(real1) and d2.dual == dual1/real1
+        d3 = op.log(d1,base=2)
+        assert d3.real == 1 and d3.dual == 1/np.log(4)
+        d4=np.log(10)
+        assert d4 == np.log(10)
+
+    def test_sqrt(self):
+        real1=4
+        dual1=1
+        d1=DualNumber(real1,dual1)
+        # test sqrt operator
+        d2 = op.sqrt(d1)
+        assert d2.real == np.sqrt(real1) and d2.dual == 1*1/2*4**(-0.5)
+        d3=op.sqrt(4)
+        assert d3==2
+        '''
+        # negative input case
+        real4=-4
+        d4=DualNumber(real4,dual1)
+        with pytest.raises(ValueError):
+            op.sqrt(d4)
+        '''
+
+    def test_sigmoid(self):
+        real1=4
+        dual1=1
+        d1=DualNumber(real1,dual1)
+        # test sigmoid operator
+        d2 = op.sigmoid(d1)
+        d3 = 1/(1+op.exp(-d1))
+        assert d2.real == d3.real and d2.dual == d3.dual
+        d4 = op.sigmoid(5)
+        assert d4 == 1/(1+np.exp(-5))
+        
 
 if __name__ == "__main__":
     pass
